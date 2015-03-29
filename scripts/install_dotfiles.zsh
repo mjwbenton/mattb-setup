1aeacc3a506cc0ad0b6124d9a3947601ce85731d
Matthew Benton <me@mattbenton.co.uk>
2015-03-29 14:35:23 -0700

    Add support for local override dotfiles to install_dotfiles.zsh

--- scripts/install_dotfiles.zsh
+++ scripts/install_dotfiles.zsh
@@ -1,19 +1,32 @@
 #!zsh -e
-setopt shwordsplit
-cd ${0:a:h}/../
-setup_path=`pwd`/dotfiles/
 
-dotfiles=$(find $setup_path -mindepth 1 -maxdepth 1 -not -path "*.git*" -not -exec basename {} \;)
-for file in $dotfiles; do
-    out_file=~/.$file
-    in_file=$setup_path$file
-    if [ -h $out_file ] ; then
-        echo "Deleting existing symlink: $out_file"
-        rm $out_file
-    fi
-    if [ -f $out_file ]; then
-        echo "Backed up existing: $out_file -> $out_file.bak"
-        mv $out_file $out_file.bak
-    fi
-    ln -s $in_file $out_file
-done;
+# mattb-setup root directory is always the directory above this script
+cd ${0:a:h}/../
+
+# Install the standard dotfiles first, then overwrite them with any local overrides
+standard_path=`pwd`/dotfiles/
+local_path=`pwd`/dotfiles.local/
+
+function install_home_symlinks() {
+    setopt shwordsplit
+    file_path=$1
+    files=$(find $file_path -mindepth 1 -maxdepth 1 -not -path "*.git*" -not -exec basename {} \;)
+    for file in $files; do
+        out_file=~/.$file
+        in_file=$file_path$file
+        if [ -h $out_file ] ; then
+            echo "Deleting existing symlink: $out_file"
+            rm $out_file
+        fi
+        if [ -f $out_file ]; then
+            echo "Backed up existing: $out_file -> $out_file.bak"
+            mv $out_file $out_file.bak
+        fi
+        ln -s $in_file $out_file
+    done;
+}
+
+echo "Standard dotfiles installing..."
+install_home_symlinks $standard_path
+echo "Local override dotfiles installing..."
+install_home_symlinks $local_path
