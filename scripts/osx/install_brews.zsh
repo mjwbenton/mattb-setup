a92831f64b0a6b27a6656491bd6bf201ccb32cba
Matt Benton <me@mattbenton.co.uk>
2015-03-28 20:12:51 -0700

    Move OSX specific scripts into osx subfolder

--- scripts/osx/install_brews.zsh
+++ scripts/osx/install_brews.zsh
@@ -2,6 +2,6 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Make it so that we can use our Brewfile
 brew tap 'homebrew/brewdler'
-# Need to make sure I'm in the folder above this script, which is where the Brewfile is
-cd ${0:a:h}/../
+# Need to make sure I'm in the folder of this script, which is where the Brewfile is
+cd ${0:a:h}/
 brew brewdle
--- scripts/setup_python.zsh
+++
@@ -1,8 +0,0 @@
-#!zsh -e
-if [ `which python` != /usr/local/bin/python ]; then
-    echo "not using brew python! - exiting"
-    exit 1
-fi
-pip install --upgrade setuptools
-pip install --upgrade pip
-pip install --upgrade virtualenv
