732317622448c2d5edd3ab3faf7f06e6cc525f21
Matt Benton <me@mattbenton.co.uk>
2015-03-28 17:55:16 -0700

    Move sudo in change_shell.zsh so that the username isn't harcoded

--- scripts/change_shell.zsh
+++ scripts/change_shell.zsh
@@ -3,6 +3,6 @@ SHELLS_FILE=/etc/shells
 NEW_SHELL=/usr/local/bin/zsh
 if ! [[ $(cat $SHELLS_FILE) =~ $NEW_SHELL ]]
 then
-    echo $NEW_SHELL >> /etc/shells
+    sudo echo $NEW_SHELL >> /etc/shells
 fi
-sudo -u mattb chsh -s $NEW_SHELL
+chsh -s $NEW_SHELL
