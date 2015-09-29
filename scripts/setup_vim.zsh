47dd401ec00631a23ad930f6528fc908b66066be
Matthew Benton <me@mattbenton.co.uk>
2015-09-28 21:53:34 -0700

    Setup vim undo dir

--- scripts/setup_vim.zsh
+++ scripts/setup_vim.zsh
@@ -3,8 +3,9 @@
 # Assumes your vimrc is already in place which declares which plugins you want
 
 bundle_dir=$HOME/.vim/bundle
+undo_dir=$HOME/.vim/undo
 vundle_location=$bundle_dir/Vundle.vim
-github_location=https://github.com/gmarik/Vundle.vim.git
+vundle_github_location=https://github.com/gmarik/Vundle.vim.git
 
 # Get install directory setup
 if ! [ -d $bundle_dir ]; then
@@ -12,11 +13,17 @@ if ! [ -d $bundle_dir ]; then
     mkdir -p $bundle_dir
 fi
 
+# Get undo directory setup
+if ! [ -d $undo_dir ]; then
+    echo "undo dir doesn't exist - creating"
+    mkdir -m 700 -p $undo_dir
+fi
+
 # Install Vundle
 if [ -e $vundle_location ]; then
     echo "already installed - skipping"
 else
-    git clone $github_location $vundle_location
+    git clone $vundle_github_location $vundle_location
 fi
 
 # Install the plugins in the vimrc
