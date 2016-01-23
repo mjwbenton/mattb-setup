b35a54ace548fcb61c3e996cd5a28d041edf934e
Matthew Benton <me@mattbenton.co.uk>
2016-01-22 23:59:37 -0800

    Add echo'd message about InputMono

--- scripts/osx/install_powerline_fonts.zsh
+++ scripts/osx/install_powerline_fonts.zsh
@@ -8,3 +8,5 @@ wget --quiet https://github.com/powerline/fonts/archive/master.zip --output-docu
 unzip $zip_path -d $download_path
 bash $install_script_path
 rm -Rf $download_path
+
+echo "Download InputMono at http://input.fontbureau.com/download/index.html?customize&fontSelection=fourStyleFamily&regular=InputSans-Regular&italic=InputSans-Italic&bold=InputSans-Bold&boldItalic=InputSans-BoldItalic&a=ss&g=ss&i=serifs&l=serifs&zero=slash&asterisk=0&braces=0&line-height=1.2"
