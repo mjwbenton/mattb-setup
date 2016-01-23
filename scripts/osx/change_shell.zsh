fdcfc92c7a07f7cc55aa8a33266b9bef1b379a39
Matthew Benton <me@mattbenton.co.uk>
2016-01-23 00:13:42 -0800

    Fixing change_shell.zsh a little

--- scripts/osx/change_shell.zsh
+++ scripts/osx/change_shell.zsh
@@ -3,6 +3,7 @@ SHELLS_FILE=/etc/shells
 NEW_SHELL=/usr/local/bin/zsh
 if ! [[ $(cat $SHELLS_FILE) =~ $NEW_SHELL ]]
 then
-    sudo echo $NEW_SHELL >> /etc/shells
+    echo "Adding to /etc/shells. Root required"
+    echo $NEW_SHELL | sudo tee -a /etc/shells
 fi
 chsh -s $NEW_SHELL
