aaddc302437a22e426807365992d3ddd09661551
Matthew Benton <me@mattbenton.co.uk>
2016-01-30 15:12:21 -0800

    Fix/Improve powerline fonts script

--- scripts/osx/install_powerline_fonts.zsh
+++ scripts/osx/install_powerline_fonts.zsh
@@ -2,6 +2,7 @@
 download_path=/tmp/powerline-fonts
 zip_path=$download_path/master.zip
 install_script_path=$download_path/fonts-master/install.sh
+input_mono_url="http://input.fontbureau.com/download/index.html?customize&fontSelection=fourStyleFamily&regular=InputMono-Regular&italic=InputMono-Italic&bold=InputMono-Bold&boldItalic=InputMono-BoldItalic&a=ss&g=ss&i=serifs&l=serifs&zero=slash&asterisk=0&braces=0&preset=monaco&line-height=1.2&email="
 
 mkdir $download_path
 wget --quiet https://github.com/powerline/fonts/archive/master.zip --output-document $zip_path
@@ -9,4 +10,6 @@ unzip $zip_path -d $download_path
 bash $install_script_path
 rm -Rf $download_path
 
-echo "Download InputMono at http://input.fontbureau.com/download/index.html?customize&fontSelection=fourStyleFamily&regular=InputSans-Regular&italic=InputSans-Italic&bold=InputSans-Bold&boldItalic=InputSans-BoldItalic&a=ss&g=ss&i=serifs&l=serifs&zero=slash&asterisk=0&braces=0&line-height=1.2"
+echo "Download InputMono"
+open $input_mono_url
+echo "Manually set this font in iTerm. Remember to do both ASCII and non-ASCII settings!"
