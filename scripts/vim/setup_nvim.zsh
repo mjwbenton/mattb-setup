4938cea5b9e5d4718df7517fec08ea0dff79497e
Matthew Benton <me@mattbenton.co.uk>
2016-03-09 21:51:07 -0800

    Fix setup_nvim.zsh

--- scripts/vim/setup_nvim.zsh
+++ scripts/vim/setup_nvim.zsh
@@ -1,5 +1,6 @@
 #!zsh -e
 # Requires setup_vim.zsh to have been run
-ln -s ~/.vim/ ~/.config/nvim/
+mkdir -p ~/.config/
+ln -s ~/.vim/ ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 pip2 install neovim
