22f9d9279ab9280671e3e604f9f6d0d3b5abc01b
Matthew Benton <m@mattb.tech>
2017-07-14 00:26:05 -0700

    Try out light theme for a while

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -1,12 +1,8 @@
 module.exports = {
   config: {
-    fontSize: 12,
+    fontSize: 13,
     fontFamily: '"Fira Code", "Meslo LG M DZ for Powerline", "Menlo", monospace',
-    cursorColor: 'rgba(240,242,53,0.7)',
     cursorShape: 'BLOCK',
-    foregroundColor: '#fff',
-    backgroundColor: '#111',
-    borderColor: '#222',
     css: '',
     termCSS: `
         x-screen x-row {
@@ -14,36 +10,16 @@ module.exports = {
         }
     `,
     padding: '0px',
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
     shell: '',
     shellArgs: ['--login'],
     env: {},
     bell: false,
-    copyOnSelect: false
+    copyOnSelect: false,
+    enableVibrancy: true
   },
 
   plugins: [
-      'hypertile'
+      'hyperminimal',
+      'hyper-one-light'
   ],
-
-  // `~/.hyper_plugins/local/`
-  localPlugins: [
-  ]
 };
