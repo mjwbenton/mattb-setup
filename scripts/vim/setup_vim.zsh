fcc52c880e5ad9981159d803a0a47b6a4c3fa8f6
Matthew Benton <m@mattb.tech>
2019-05-04 09:30:33 +0100

    Migrate to vim-plug

--- scripts/vim/setup_vim.zsh
+++ scripts/vim/setup_vim.zsh
@@ -2,29 +2,15 @@
 
 # Assumes your vimrc is already in place which declares which plugins you want
 
-bundle_dir=$HOME/.vim/bundle
-undo_dir=$HOME/.vim/undo
-vundle_location=$bundle_dir/Vundle.vim
-vundle_github_location=https://github.com/gmarik/Vundle.vim.git
-
-# Get install directory setup
-if ! [ -d $bundle_dir ]; then
-    echo "bundle dir doesn't exist - creating"
-    mkdir -p $bundle_dir
-fi
+# Install vim-plug
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Get undo directory setup
+undo_dir=$HOME/.vim/undo
 if ! [ -d $undo_dir ]; then
-    echo "undo dir doesn't exist - creating"
     mkdir -m 700 -p $undo_dir
 fi
 
-# Install Vundle
-if [ -e $vundle_location ]; then
-    echo "already installed - skipping"
-else
-    git clone $vundle_github_location $vundle_location
-fi
-
 # Install the plugins in the vimrc
-vim +PluginInstall +qall
+vim +PlugInstall +qall
