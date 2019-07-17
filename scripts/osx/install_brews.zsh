007bb53f58314b725bc2d663bd00c46c625a68c3
Matthew Benton <m@mattb.tech>
2019-07-17 08:49:36 +0100

    Fix install_brews script

--- scripts/osx/install_brews.zsh
+++ scripts/osx/install_brews.zsh
@@ -1,5 +1,5 @@
-#!zsh -e
+#!/usr/bin/env bash
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Need to make sure I'm in the folder of this script, which is where the Brewfile is
 cd ${0:a:h}/
-brew bundle
+brew bundle --file=./Brewfile
