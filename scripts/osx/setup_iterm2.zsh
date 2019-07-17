e44a9e99d094cfd9d601d4f91ef650fbd7ea4a1a
Matthew Benton <m@mattb.tech>
2019-07-17 08:51:01 +0100

    Fix setup_iterm2 script for being run when iterm hasn't been started before

--- scripts/osx/setup_iterm2.zsh
+++ scripts/osx/setup_iterm2.zsh
@@ -2,6 +2,7 @@
 
 # This script requires you to have already setup dotfiles and installed iterm2
 
-rmdir ~/Library/Application\ Support/iTerm2/DynamicProfiles
+rm -rf ~/Library/Application\ Support/iTerm2/DynamicProfiles
+mkdir -p ~/Library/Application\ Support/iTerm2
 ln -s ~/.iterm2_profiles ~/Library/Application\ Support/iTerm2/DynamicProfiles
 defaults write com.googlecode.iterm2 "Default Bookmark Guid" -string "BFFDEB92-1D93-4D34-AA0D-11308E9209AE"
