e5ca3662070e7d0f486f8306dcf4d788502aa17d
Matthew Benton <m@mattb.tech>
2021-05-31 21:55:02 +0100

    Basic fish aliases

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,10 @@
-fish_vi_key_bindings
-set -g async_prompt_functions _pure_prompt_git
-set -g pure_enable_single_line_prompt true
+# Basic setup
 
+fish_vi_key_bindings
+set -g pure_enable_single_line_prompt true
+set -g async_prompt_functions _pure_prompt_git
+
+# Aliases
+
+alias vim "nvim"
+alias glog "git log --oneline --decorate --graph --all"
