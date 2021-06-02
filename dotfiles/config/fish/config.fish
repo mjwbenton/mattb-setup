f5e93011bd1b3a278189cc3b1697c24f686d2666
Matthew Benton <m@mattb.tech>
2021-06-02 21:09:32 +0100

    Remove zsh configuration

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,4 @@
-# Basic setup
+# Fish configuration
 
 fish_vi_key_bindings
 set -g pure_enable_single_line_prompt true
@@ -8,4 +8,9 @@ set -g async_prompt_functions _pure_prompt_git
 
 alias vim "nvim"
 alias glog "git log --oneline --decorate --graph --all"
+alias tar "gtar"
 
+# Environment variables
+
+set VISUAL "nvim"
+set EDITOR $VISUAL
--- scripts/install_perlbrew.zsh
+++
@@ -1,4 +0,0 @@
-#!zsh -e
-curl -L http://install.perlbrew.pl | bash
-~/perl5/perlbrew/bin/perlbrew install-cpanm
-echo "install a perl version with 'perlbrew install perl-5.xx.x'"
