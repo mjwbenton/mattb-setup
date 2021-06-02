8b15d230d7005806413fee19fbddd91415fdb117
Matthew Benton <m@mattb.tech>
2021-06-02 21:18:23 +0100

    Remove iterm2

--- scripts/osx/setup_iterm2.zsh
+++
@@ -1,8 +0,0 @@
-#!zsh -e
-
-# This script requires you to have already setup dotfiles and installed iterm2
-
-rm -rf ~/Library/Application\ Support/iTerm2/DynamicProfiles
-mkdir -p ~/Library/Application\ Support/iTerm2
-ln -s ~/.iterm2_profiles ~/Library/Application\ Support/iTerm2/DynamicProfiles
-defaults write com.googlecode.iterm2 "Default Bookmark Guid" -string "BFFDEB92-1D93-4D34-AA0D-11308E9209AE"
