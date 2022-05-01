5b777bb7c3076061a7e348929e9c9117f2110fd5
Matthew Benton <m@mattb.tech>
2022-05-01 19:24:59 +0100

    Update install_brews

--- scripts/osx/install_brews.zsh
+++ scripts/osx/install_brews.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Need to make sure I'm in the folder of this script, which is where the Brewfile is
 cd ${0:a:h}/
-brew bundle --file=./Brewfile
+/opt/homebrew/bin/brew bundle --file=./Brewfile
