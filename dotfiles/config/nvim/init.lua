550dc704bb64e9c14ad5ffc6a959835dbbd26ddb
Matthew Benton <m@mattb.tech>
2025-12-03 12:28:30 +0000

    prettier

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -47,6 +47,31 @@ require("lazy").setup({
           indent = { enable = true },
         })
       end,
+    },
+    {
+      'stevearc/conform.nvim',
+      event = { 'BufWritePre' },
+      cmd = { 'ConformInfo' },
+      config = function()
+        require('conform').setup({
+          formatters_by_ft = {
+            javascript = { 'prettier' },
+            typescript = { 'prettier' },
+            javascriptreact = { 'prettier' },
+            typescriptreact = { 'prettier' },
+            css = { 'prettier' },
+            html = { 'prettier' },
+            json = { 'prettier' },
+            yaml = { 'prettier' },
+            markdown = { 'prettier' },
+            graphql = { 'prettier' },
+          },
+          format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true,
+          },
+        })
+      end,
     }
   },
   install = { colorscheme = { "habamax" } },
