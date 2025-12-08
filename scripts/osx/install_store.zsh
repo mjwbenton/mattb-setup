60d3f4dbf4d982cb7866d82651438419201fa83a
Matthew Benton <m@mattb.tech>
2025-12-08 21:51:49 +0000

    More store apps

--- scripts/osx/install_store.zsh
+++ scripts/osx/install_store.zsh
@@ -1,2 +1,11 @@
 #!zsh -e
+# Strongbox
 mas install 897283731
+# Shush
+mas install 496437906
+# Xcode
+mas install 497799835
+# Safari: AdBlock Plus
+mas install 1432731683
+# Safari: Hush
+mas install 1544743900
