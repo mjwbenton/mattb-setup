cca6a8fb1ca1f8154dbf4a70da38331828308dac
Matthew Benton <m@mattb.tech>
2025-12-08 22:31:46 +0000

    update fisher installer

--- scripts/install_fisher.fish
+++ scripts/install_fisher.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
-curl -sL https://git.io/fisher | source
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher update
