6663c186a3489184886dbf65a3fa384d73825faf
Matthew Benton <m@mattb.tech>
2025-12-08 21:57:31 +0000

    Remove cursor setup

--- scripts/install_cursor_extensions.zsh
+++
@@ -1,3 +0,0 @@
-#!zsh -e
-
-xargs -n1 cursor --install-extension < ~/.config/cursor/vscode-extensions.txt
--- scripts/osx/setup_cursor.zsh
+++
@@ -1,4 +0,0 @@
-#!zsh -e
-
-rm -rf ~/Library/Application\ Support/Cursor/User
-ln -s ~/.config/cursor ~/Library/Application\ Support/Cursor/User
