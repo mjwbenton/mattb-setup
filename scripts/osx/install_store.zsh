8a7a10287d4e5cfb4583681198acf1ec787d8929
Matthew Benton <m@mattb.tech>
2025-12-10 10:17:21 +0000

    Install xcode last

--- scripts/osx/install_store.zsh
+++ scripts/osx/install_store.zsh
@@ -3,11 +3,11 @@
 mas install 897283731
 # Shush
 mas install 496437906
-# Xcode
-mas install 497799835
 # Safari: AdBlock Plus
 mas install 1432731683
 # Safari: Hush
 mas install 1544743900
 # Numbers
 mas install 409203825
+# Xcode
+mas install 497799835
